f8b306608063108035a6b3a93310d8bced248214
vasilek3 <163405994+vasilek3@users.noreply.github.com>
2024-12-13 19:02:23 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# Input two arrays as strings
+signals = list(map(int, input().split(", ")))  # First input array as a string
+weights = list(map(int, input().split(", ")))  # Second input array as a string
+
+def max_signals_amount(signals, weights):
+    modified_signals = []
+    for i in range(len(signals)):
+        modified_signals.append(signals[i] * weights[i])
+    print(modified_signals)
+    max_number = -10 ** 9
+    for start in range(len(modified_signals)):
+        neutral_number = 1
+        for end in range(start, len(modified_signals)):
+            neutral_number *= modified_signals[end]
+            max_number = max(max_number, neutral_number)
+
+    return max_number
+
+print(max_signals_amount(signals, weights))
+
+#voloshka_3 / vasilek_3 CTF2024 University Badlands
